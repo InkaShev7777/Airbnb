e5b1363a3dc6e653432044aae00bcb6b07acab06
Ilya Shevchenko <95874401+InkaShev7777@users.noreply.github.com>
2024-08-12 14:00:09 +0300

    create search and filter bar

---
+++ Airbnb/Core/Listnings/ListingItemView.swift
@@ -0,0 +1,71 @@
+//
+//  ListingView.swift
+//  Airbnb
+//
+//  Created by Ilya Schevchenko on 12.08.2024.
+//
+
+import SwiftUI
+
+struct ListingItemView: View {
+    
+    var images = [
+        "listing-1",
+        "listing-2",
+        "listing-3",
+        "listing-4",
+    ]
+    
+    var body: some View {
+        VStack(spacing: 8) {
+            // images
+            TabView {
+                ForEach(images, id: \.self) { image in
+                   Image(image)
+                        .resizable()
+                        .scaledToFill()
+                }
+            }
+            .frame(height: 320)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .tabViewStyle(.page)
+            
+            //listing details
+            
+            HStack(alignment: .top) {
+                //details
+                VStack(alignment: .leading) {
+                    Text("Miami, Florida")
+                        .fontWeight(.semibold)
+                    
+                    Text("12 mi away")
+                        .foregroundStyle(.gray)
+                    
+                    Text("Nov 3 - 10")
+                        .foregroundStyle(.gray)
+                    
+                    HStack(spacing: 4) {
+                        Text("$567")
+                            .fontWeight(.semibold)
+                        Text("night")
+                    }
+                }
+                
+                Spacer()
+                
+                //rating
+                
+                HStack(spacing: 2) {
+                    Image(systemName: "star.fill")
+                    
+                    Text("4.86")
+                }
+            }
+            .font(.footnote)
+        }
+    }
+}
+
+#Preview {
+    ListingItemView()
+}
