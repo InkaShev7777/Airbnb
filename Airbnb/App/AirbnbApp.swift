f2379fbb83d940271da90f162006e4daf6709217
Ilya Shevchenko <95874401+InkaShev7777@users.noreply.github.com>
2024-08-12 12:14:03 +0300

    Add base struct for app

---
+++ Airbnb/App/AirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbApp.swift
+//  Airbnb
+//
+//  Created by Ilya Schevchenko on 12.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Airbnb/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Airbnb
+//
+//  Created by Ilya Schevchenko on 12.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
